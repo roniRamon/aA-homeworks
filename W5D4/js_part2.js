639378721a0dcb5fe6b1162ee50c5d6da4a6af7f
RoniRamon <roniramon260@gmail.com>
2018-03-14 22:28:14 -0700

    last function in HM

--- W5D4/js_part2.js
+++ W5D4/js_part2.js
@@ -50,5 +50,11 @@ herd.forEach(elephant => Elephant.paradeHelper(elephant));
 
 //closures
 function dinerBreakfast(){
+  let order = "I'd like cheesy scrambled eggs please.";
+  console.log(order);
 
+  return function (food) {
+    order = `${order.slice(0, order.length - 8)} and ${food} please.`;
+    console.log(order);
+  };
 }
