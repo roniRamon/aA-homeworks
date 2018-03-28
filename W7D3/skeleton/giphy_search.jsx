87adc50107a4677945bdde18710412fc34d7b076
RoniRamon <roniramon260@gmail.com>
2018-03-27 21:09:14 -0700

    Giphy home work

---
+++ W7D3/skeleton/giphy_search.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+
+import { receiveSearchGiphys, fetchSearchGiphys } from './actions/giphy_actions';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+  // window.fetchSearchGiphys = fetchSearchGiphys;
+  // window.receiveSearchGiphys = receiveSearchGiphys;
+  // window.store = store;
+});
